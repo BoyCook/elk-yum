745a53bc2cd25ef3432ee33b3ac3fbefc1c03817
Craig Cook <craig.cook@code.red>
2017-07-08 10:43:00 +0100

    added logging to setup file

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+echo 'Download and install the public signing key:'
+rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
+
+echo 'Copying files into /etc/yum.repos.d/[elasticsearch.repo|kibana.repo|logstash.repo]'
 cp elasticsearch.repo /etc/yum.repos.d/
 cp kibana.repo /etc/yum.repos.d/
 cp logstash.repo /etc/yum.repos.d/
+
+sudo yum install elasticsearch
+sudo yum install kibana
+sudo yum install logstash
